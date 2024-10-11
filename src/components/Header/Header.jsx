8f21b63a848f6dd2c754ab1ae62c7ca6cd4cc8ec
samarth kumar <samarthgupta316@gmail.com>
2024-10-11 21:05:20 +0530

    Add files via upload

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Header.css"
+const header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your Favourite food Here</h2>
+        <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest materials.</p>
+        <button>View Menu</button>
+      
+      </div>
+    </div>
+  )
+}
+
+export default header
